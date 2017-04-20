448f904d4299eed4f7290557a7dd2ed03a731583
Michael Elliott <elliomic@onid.orst.edu>
2017-04-20 16:39:15 -0700

    Added actual header file for Mersenne Twister

---
+++ mt19937ar.h
@@ -0,0 +1,20 @@
+#ifndef MERSENNE_TWISTER_H
+#define MERSENNE_TWISTER_H
+
+void init_genrand(unsigned long);
+
+void init_by_array(unsigned long[], int);
+
+unsigned long genrand_int32(void);
+
+long genrand_int31(void);
+
+double genrand_real1(void);
+
+double genrand_real2(void);
+
+double genrand_real3(void);
+
+double genrand_res53(void);
+
+#endif
