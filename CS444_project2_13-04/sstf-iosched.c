c3e76fdc0692f62eda1eb70f4d37086fe0091943
Michael Elliott <elliomic@onid.orst.edu>
2017-05-15 20:45:26 -0700

    Finished hw2

---
+++ CS444_project2_13-04/sstf-iosched.c
@@ -0,0 +1,159 @@
+/*
+ * elevator SSTF
+ */
+#include <linux/blkdev.h>
+#include <linux/elevator.h>
+#include <linux/bio.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+
+struct sstf_data {
+	struct list_head queue;
+	sector_t head_pos;
+};
+
+static void sstf_merged_requests(struct request_queue *q, struct request *rq,
+				 struct request *next)
+{
+	list_del_init(&next->queuelist);
+}
+
+static int sstf_dispatch(struct request_queue *q, int force)
+{
+	struct sstf_data *sd = q->elevator->elevator_data;
+
+	if (!list_empty(&sd->queue)) {
+		struct request *rq;
+		rq = list_entry(sd->queue.next, struct request, queuelist);
+		list_del_init(&rq->queuelist);
+		sd->head_pos = blk_rq_pos(rq) + blk_rq_sectors(rq);
+		elv_dispatch_add_tail(q, rq);
+		return 1;
+	}
+	return 0;
+}
+
+
+static void sstf_add_request(struct request_queue *q, struct request *rq)
+{
+	struct sstf_data *sd = q->elevator->elevator_data;
+
+	if (list_empty(&sd->queue)) {
+		list_add_tail(&rq->queuelist, &sd->queue);
+	} else {
+		struct request *rq_cur;
+		unsigned long rq_dist;
+
+		rq_dist = abs(blk_rq_pos(rq) - sd->head_pos);
+		rq_cur = list_entry(sd->queue.next, struct request, queuelist);
+
+		while (rq_dist < abs(blk_rq_pos(rq_cur) - sd->head_pos)) {
+			rq_cur = list_entry(sd->queue.next, struct request, queuelist);;
+		}
+
+		list_add_tail(&rq->queuelist, &rq_cur->queuelist);
+	}
+}
+
+/*
+ * Returns the request in the queue before the given request.
+ */
+static struct request *
+sstf_former_request(struct request_queue *q, struct request *rq)
+{
+	struct sstf_data *sd = q->elevator->elevator_data;
+
+	if (rq->queuelist.prev == &sd->queue)
+		return NULL;
+	return list_entry(rq->queuelist.prev, struct request, queuelist);
+}
+
+/*
+ * Returns the request in the queue after the given request.
+ */
+static struct request *
+sstf_latter_request(struct request_queue *q, struct request *rq)
+{
+	struct sstf_data *sd = q->elevator->elevator_data;
+
+	if (rq->queuelist.next == &sd->queue)
+		return NULL;
+	return list_entry(rq->queuelist.next, struct request, queuelist);
+}
+
+/*
+ * Initializes the queue.
+ */
+static int sstf_init_queue(struct request_queue *q, struct elevator_type *e)
+{
+	struct sstf_data *sd;
+	struct elevator_queue *eq;
+
+	eq = elevator_alloc(q, e);
+	if (!eq)
+		return -ENOMEM;
+
+	sd = kmalloc_node(sizeof(*sd), GFP_KERNEL, q->node);
+	if (!sd) {
+		kobject_put(&eq->kobj);
+		return -ENOMEM;
+	}
+	eq->elevator_data = sd;
+
+	INIT_LIST_HEAD(&sd->queue);
+
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+	return 0;
+}
+
+/*
+ * Closes the queue. Panics the kernel if queue is not empty.
+ */
+static void sstf_exit_queue(struct elevator_queue *e)
+{
+	struct sstf_data *sd = e->elevator_data;
+
+	BUG_ON(!list_empty(&sd->queue));
+	kfree(sd);
+}
+
+static struct elevator_type elevator_sstf = {
+	.ops = {
+		.elevator_merge_req_fn		= sstf_merged_requests,
+		.elevator_dispatch_fn		= sstf_dispatch,
+		.elevator_add_req_fn		= sstf_add_request,
+		.elevator_former_req_fn		= sstf_former_request,
+		.elevator_latter_req_fn		= sstf_latter_request,
+		.elevator_init_fn		= sstf_init_queue,
+		.elevator_exit_fn		= sstf_exit_queue,
+	},
+	.elevator_name = "SSTF",
+	.elevator_owner = THIS_MODULE,
+};
+
+/*
+ * Initializes the elevator.
+ */
+static int __init sstf_init(void)
+{
+	return elv_register(&elevator_sstf);
+}
+
+/*
+ * Exits the elevator.
+ */
+static void __exit sstf_exit(void)
+{
+	elv_unregister(&elevator_sstf);
+}
+
+module_init(sstf_init);
+module_exit(sstf_exit);
+
+
+MODULE_AUTHOR("Michael Elliott");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("SSTF IO scheduler");
